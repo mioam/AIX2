033100a78c1bbbe0f7efc3730c776b71345d9fb7
miaom <1678068845@qq.com>
2021-12-02 13:59:05 +0800

    a

--- train_ner.py
+++ train_ner.py
@@ -20,7 +20,26 @@ optimzer = torch.optim.AdamW(net.parameters(),lr=lr,weight_decay=1e-5)
 
 loss_fn = nn.CrossEntropyLoss()
 
+@torch.no_grad()
+def eva(net, test):
+    test_loader = torch.utils.data.DataLoader(test, batch_size=32)
+    
+    s = []
+    m = torch.zeros((7,7))
+    for feature, label in test_loader:
+        feature = feature.to(_global.device)
+        label = label.to(_global.device)
+        out = net(feature)
+        loss = loss_fn(out,label)
+        for i in range(out.shape[0]):
+            a = out[i].argmax().item()
+            b = label[i].item()
+            m[a,b] += 1
+    return sum(s) / len(s), m
+
 for epoch in range(num_epoch):
+    net.train()
+    s = []
     for feature, label in train_loader:
         feature = feature.to(_global.device)
         label = label.to(_global.device)
@@ -29,4 +48,9 @@ for epoch in range(num_epoch):
         optimzer.zero_grad()
         loss.backward()
         optimzer.step()
-        print(loss.item())
+        # print(loss.item())
+        s.append(loss.item())
+    s, m = eva(net, test)
+    print(f'epoch: {epoch}, loss: {sum(s) / len(s)}')
+    print(s)
+    print(m)
