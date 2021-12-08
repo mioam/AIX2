4ae74a69d0bb260420ee9bc59346c30efbb72577
miaom <1678068845@qq.com>
2021-12-08 15:25:14 +0800

    test

--- utils/test.py
+++ utils/test.py
@@ -2,25 +2,28 @@ import _global
 from utils.load import Anhao, AllRelation, Text
 from utils.examples import example
 
-from models.classifier import Net, AttnNet, ClsNet
+from models.classifier import Net, AttnNet, ClsNet, AttnBertNet
 import torch
 import os
 from tqdm import tqdm
 
 @torch.no_grad()
-def predict(model, relation):
+def predict(model, sample):
     model.eval()
-    ret = []
-    for x, y in relation:
-        output = model([x],[y])
-        # print(output)
-        ret.append(output[0].argmax().item())
-    return ret
+    # ret = []
+    x, y, ex = [a[0] for a in sample], [a[1] for a in sample], [a[2] for a in sample]
+    output = model(x,y,ex)
+    ans = output.argmax(1).cpu()
+    # for i, _ in enumerate(sample):
+    #     ret.append(output[i].argmax().item())
+    return ans
 
 if __name__ == '__main__':
 
-    model = AttnNet(96, 8 ,4)
-    path = './checkpoints/Attn.pt'
+    # model = AttnNet(96, 8 ,4)
+    # path = './checkpoints/Attn.pt'
+    model = AttnBertNet(96, 8 ,4)
+    path = './checkpoints/AttnBert.pt'
     X = torch.load(path,map_location=_global.device)
     model.load_state_dict(X['model'])
     model.to(_global.device)
@@ -45,27 +48,31 @@ if __name__ == '__main__':
         
         # cnt_pT = 0
         # cnt_nT = 0
-        for x in p:
-            label = 0
-            if check(id,x[0]):
-                anhao = 1
-            else:
-                anhao = 0
-                o1.append((id,x[0]))
-            pred = predict(model, [(text[id],text[x[0]]), ])[0]
-            cnt[label,pred,anhao] += 1
-        for x in n:
-            label = 1
-            if check(id,x[0]):
-                if len(a[id]) and len(a[x[0]]) and a[id][0] == a[x[0]][0]:
-                    anhao = 2
-                else:
+        if len(p):
+            pred = predict(model, [(text[id], text[x[0]], (id, x[0])) for x in p])
+            for i, x in enumerate(p):
+                label = 0
+                if check(id,x[0]):
                     anhao = 1
-                    o2.append((id,x[0]))
-            else:
-                anhao = 0
-            pred = predict(model, [(text[id],text[x[0]]), ])[0]
-            cnt[label,pred,anhao] += 1
+                else:
+                    anhao = 0
+                    o1.append((id,x[0]))
+                # pred = predict(model, [(text[id],text[x[0]]), ])[0]
+                cnt[label,pred[i],anhao] += 1
+        
+        if len(n):
+            pred = predict(model, [(text[id], text[x[0]], (id, x[0])) for x in n])
+            for i, x in enumerate(n):
+                label = 1
+                if check(id,x[0]):
+                    if len(a[id]) and len(a[x[0]]) and a[id][0] == a[x[0]][0]:
+                        anhao = 2
+                    else:
+                        anhao = 1
+                        o2.append((id,x[0]))
+                else:
+                    anhao = 0
+                cnt[label,pred[i],anhao] += 1
 
         # if cnt_nT > 10:
         #     print(cnt_pT, cnt_nT)
