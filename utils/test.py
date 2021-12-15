bc91b5770649135d0bd56212dfe5068d9701cf24
miaom <1678068845@qq.com>
2021-12-15 13:36:54 +0800

    a

--- utils/test.py
+++ utils/test.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
                 # else:
                 #     anhao = 0
                 cnt[label,pred[i],anhao] += 1
-        break
+        # break
         # if cnt_nT > 10:
         #     print(cnt_pT, cnt_nT)
         #     print(a[id])
